65f21004b880f8b4123e39a08ca598fc8c2db7f6
Andriy <andygrigs88@gmail.com>
2025-09-05 17:38:28 +0200

    add models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Date, Text
+from database import Base
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(51), nullable=False, index=True)
+    last_name = Column(String(51), nullable=False, index=True)
+    email = Column(String(102), unique=True,nullable=False, index=True)
+    phone_number = Column(String(21), nullable=False)
+    birth_date = Column(Date, nullable=False)
+    additional_info= Column(Text, nullable=True)
